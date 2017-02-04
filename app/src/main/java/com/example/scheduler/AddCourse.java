bbe03ac60a6fa279913329ad4e370ba6dc8db2ac
oboekevin <kevinchaplin58@gmail.com>
2017-02-03 19:27:51 -0500

    Init
    Make a few buttons, start the thing

---
+++ app/src/main/java/com/example/scheduler/AddCourse.java
@@ -0,0 +1,13 @@
+package com.example.scheduler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddCourse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_course);
+    }
+}
