33b28382e279596c19b2ed635a1a8667c8960349
oboekevin <kevinchaplin58@gmail.com>
2017-02-03 19:53:41 -0500

    Add README

---
+++ README.md
@@ -0,0 +1,9 @@
+Scheduler
+=========
+
+To do:
+------
+- Flesh out add course page
+- Get from ^ page back to first one
+- (With the information)
+- Get data?
--- app/src/main/java/com/example/scheduler/AddCourse.java
+++ app/src/main/java/com/example/scheduler/AddCourse.java
@@ -2,6 +2,13 @@ package com.example.scheduler;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.widget.TextView;
+
+import static com.example.scheduler.R.id.editText;
+import static com.example.scheduler.R.id.editText2;
 
 public class AddCourse extends AppCompatActivity {
 
